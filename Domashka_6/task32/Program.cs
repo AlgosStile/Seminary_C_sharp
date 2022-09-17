caa80779d1fb55f1ed058b9a170b69806ce86a62
Oleg Todor <oleg.todor@mail.ru>
2022-09-17 10:25:30 +0300

    добавил папку Domaska_6 с 3-я задачами

---
+++ Domashka_6/task32/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 40: Напишите программу, которая принимает на вход три числа
+// и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника 
+// меньше суммы двух других сторон.
+
+// ( НЕОБЯЗАТЕЛЬНАЯ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
+
+Console.Write("Введите строну а: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Введите строну b: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("Введите строну c: ");
+        double c = double.Parse(Console.ReadLine());
+        if ((a + b > c) && (b + c > a) && (a + c > b))
+        {
+            double p = (a + b + c) / 2;
+            Console.WriteLine("Это треугольник");
+            Console.WriteLine("Площадь треугольника: " + Math.Sqrt(p * (p - a) * (p - b) * (p - c)));
+        }
+        else
+        {
+            Console.WriteLine("Это не треугольник");
+        }
+        Console.ReadKey();
+
---
+++ Domashka_6/task33/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine ("Введите числа:");
+string numbers = Console.ReadLine();
+
+string [] newNumbers = new string [numbers.Length];
+int k = 0;
+for (int i = 0; i < newNumbers.Length; i++)
+{
+  if (numbers[i] == ',')
+  {
+    k++;
+  }
+  else
+  {
+    newNumbers[k] = newNumbers[k]+$"{numbers[i]}";
+  }
+}
+k++;
+int [] resultNumbers = new int[k];
+PrintArray (resultNumbers, newNumbers);
+int sum = 0;
+for (int i = 0; i < k; i++)
+{
+  if (resultNumbers[i]>0)
+  {
+    sum++;
+  }  
+}
+
+Console.WriteLine ("Количество чисел больше 0: "+sum);
+
+void PrintArray (int [] array, string [] string1)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+  array[i] = Convert.ToInt32(string1[i]);
+  Console.Write (array[i]+"  ");
+  
+  }
+  
+Console.WriteLine();
+
+}
+
+// p.s. Иван, я не совсем понял какое значение нужно ввести, чтобы при помощи слова
+// "stop" останавливался ввод чисел 
+//     Должно быть что-то типа этого:
+//     if(answer == "Stop")
+// {
+//     break;
+// }      
+// Так ?
